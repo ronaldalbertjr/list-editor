6347f85092856a672f2052c2ae72dd80f772936d
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-08-18 09:14:26 -0300

    ItemList
    Example of showing and modidying a list in unity editor

---
+++ Assets/Editor/TableScriptEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(TableScript))]
+public class TableScriptEditor : Editor
+{
+    int indexToRemove;
+    public override void OnInspectorGUI()
+    {
+        indexToRemove = -1;
+        TableScript table = target as TableScript;
+        Undo.RecordObject(table, "Table Changed");
+        EditorGUI.BeginChangeCheck();
+        foreach (Item i in table.lista)
+        {
+            EditorGUILayout.BeginHorizontal();
+            i.key = EditorGUILayout.TextField(i.key);
+            i.value = EditorGUILayout.IntField(i.value);
+            if(GUILayout.Button("Remove"))
+                indexToRemove = table.lista.IndexOf(i);
+            EditorGUILayout.EndHorizontal();
+        }
+        if(indexToRemove >= 0)
+            table.lista.RemoveAt(indexToRemove);
+        if (GUILayout.Button("Add Item", GUILayout.Height(20)))
+            table.lista.Add(new Item());
+        
+    }
+}
---
+++ Assets/TableScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+[Serializable]
+public class Item
+{
+    public string key = "New Key";
+    public int value;
+}
+
+[ExecuteInEditMode]
+public class TableScript : MonoBehaviour
+{
+    public List<Item> lista = new List<Item>();
+    
+}
